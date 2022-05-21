480505b7ffa268a856cb14074da3f58c10163396
mathilde-chenillot <mathilde.cheni@gmail.com>
2022-05-21 18:28:37 +0200

    changes in migrations for unique field and models

--- database/migrations/1652784482300_topics.ts
+++ database/migrations/1652784482300_topics.ts
@@ -6,7 +6,7 @@ export default class Topics extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('title', 25).notNullable()
+      table.string('title', 25).notNullable().unique()
       table.string('description', 255).notNullable()
       table.integer('user_id').notNullable()
       table.timestamps(true)
