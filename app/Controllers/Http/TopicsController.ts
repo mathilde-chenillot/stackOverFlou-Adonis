5e78b1a716e742ec08d98536fa76a624c112e75b
mathilde-chenillot <mathilde.cheni@gmail.com>
2022-05-26 20:09:56 +0200

    read one method ok

--- app/Controllers/Http/TopicsController.ts
+++ app/Controllers/Http/TopicsController.ts
@@ -1,11 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Topic from 'App/Models/Topic'
+import User from 'App/Models/User'
 
 export default class TopicsController {
-  // To read
+  // To read all
 
   public async index({ request, response }: HttpContextContract) {
     const topics = await Topic.all()
+    return response.ok(topics)
+  }
+
+  //To read one
+
+  public async show({ request, response, params }: HttpContextContract) {
+    const topics = await Topic.findOrFail(params.id)
     return response.ok(topics)
   }
 }
